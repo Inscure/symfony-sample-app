f9a9dffc514a934c44426071278038cd488b91b8
Inscure <drimer.eco@gmail.com>
2014-01-18 17:06:28 +0100

    Usuniecie zbednych pakietow

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,14 +17,13 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Acme\HelloBundle\AcmeHelloBundle(),
-            new Acme\AuthBundle\AcmeAuthBundle(),
             new Acme\UserBundle\AcmeUserBundle(),
 			new FOS\UserBundle\FOSUserBundle(),
 			new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
+            //$bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
